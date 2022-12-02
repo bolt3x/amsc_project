3eac2bafa3aed60e0deb3ed95536d60f74c59963
bolt3x <valeriofassi@hotmail.it>
2022-12-02 15:41:46 +0100

    now returnss a vector

--- fft.cpp
+++ fft.cpp
@@ -4,41 +4,36 @@
 
 using namespace std;
 
-void Recursive_FFT(vector<complex<double>> a, size_t n,vector<complex<double>>& y,double start,double end){
+vector<complex<double>> Recursive_FFT(vector<complex<double>> x, size_t n,vector<complex<double>>& y){
     if(n == 1){
-        cout << start << endl;
-        cout << end << endl;
-        y[start] = a[start];
-        return;
+        return x;
     }
 
-    complex<double> w = 1;
+    complex<double> w(1,0);
     const complex<double> w_n(std::cos(2 * 3.14 / n),std::sin(2* 3.14/ n));
 
     vector<complex<double>> even(n / 2);
     vector<complex<double>> odd(n / 2);
     
     for(size_t i = 0; i < n/2; i++){
-        even[i] = a[i*2];
-        odd[i] = a[i*2+1];
+        even[i] = x[i*2];
+        odd[i] = x[i*2+1];
     } 
 
-    vector<complex<double>> y_o(n/2);
-    Recursive_FFT(even,n/2,y_o,start,n / 2);
-    vector<complex<double>> y_e(n/2);
-    Recursive_FFT(odd,n/2,y_e,start + n / 2, end);
-
+    vector<complex<double>> y_o = Recursive_FFT(even,n/2,y);
+    vector<complex<double>> y_e = Recursive_FFT(odd,n/2,y);
     for(size_t k = 0; k < n / 2 - 1; k++){
         y[k] = y_o[k] + w * y_e[k];
+
         y[k + n/2] = y_o[k] - w * y_e[k];
+    
         w = w * w_n;
     }
-
-    return;
+    return y;
 }
 
 int main(){
-    size_t n = 10;
+    size_t n = 16;
     vector<complex<double>> input(n);
     for(size_t i = 0; i < n; i++){
         input[i] = (complex<double>(1, i));
@@ -46,8 +41,8 @@ int main(){
     }
     cout << endl;
 
-    vector<complex<double>> y(n); 
-    Recursive_FFT(input,n,y,0,n-1);
+    vector<complex<double>> y(n);
+    Recursive_FFT(input,n,y);
 
     for(size_t i = 0; i < n; i++){
         cout << y[i] << " ";
