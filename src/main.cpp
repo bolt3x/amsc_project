da4bd6878fa5f55ae6bb7fd66e09dba158aaca9b
bolt3x <valeriofassi@hotmail.it>
2022-12-12 17:12:59 +0100

    added command line options

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,85 @@
 #include <iostream>
 #include <vector>
 #include <complex>
+
+#include <unistd.h>
+
 #include "fft.hpp"
 #include "utilities.hpp"
 
-int main(){
+int main(int argc, char **argv){
+	
+	
+	int rec_flag = 0;
+	int iter_flag = 0;
+	int inv_flag = 0;
+	int par_flag = 0;
+	char *signal_value = NULL;
+	
+	int c;
+	
+	while ((c = getopt (argc, argv, "rtips:")) != -1)
+    switch (c)
+	{
+      case 'r':
+        rec_flag = 1;
+        break;
+      case 't':
+        iter_flag = 1;
+        break;
+      case 'i':
+        inv_flag = 1;
+        break;
+      case 'p':
+        par_flag = 1;
+        break;
+      case 's':
+        signal_value = optarg;
+        break;
+      case '?':
+        if (optopt == 's')
+          fprintf (stderr, "Option -%s requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%s'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+	}
+	
+	size_t n = 8;
+	std::vector<std::complex<double>> input{1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
 
-    size_t n = 8;
-    std::vector<std::complex<double>> input{1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
+	PrintIt(input,"Input");
+	FFTGenerator fft(input,n);
+	
+	if(rec_flag){
 
-    FFTGenerator fft(input,n);
-    
-    TimeIt(&FFTGenerator::Recursive_FFT,fft);
+		fft.Recursive_FFT();
 
-    fft.Iterative_FFT();
+		PrintIt(fft.getRecT(),"Recursive");
+	}
+	if(iter_flag){
 
-    PrintIt(input,"Input");
-    PrintIt(fft.getRecT(),"Recursive");
-    PrintIt(fft.getIterT(),"Iterative");
+		fft.Iterative_FFT();
 
-    input = fft.getIterT();
+		PrintIt(fft.getIterT(),"Iterative");
+	}
+	if(par_flag){
+		std::cout << "yet to be implemented..." << std::endl;
+	}
+	if(inv_flag){
+		
+		fft.Inverse_FFT();
 
-    FFTGenerator ifft(input,n);
+		PrintIt(fft.getInvT(),"Inverse");
+	}
+	
 
-    ifft.Inverse_FFT();
 
-    PrintIt(input,"Inverse Input");
-    PrintIt(ifft.getInvT(),"Inverse");
 
     return 0;
 
